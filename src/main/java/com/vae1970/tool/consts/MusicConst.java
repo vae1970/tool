1d596510740572c39bc6378b3da4043d60fc8bea
gudz <496075305@qq.com>
2019-10-30 09:24:46 +0800

    docs: 接口文档

--- src/main/java/com/vae1970/tool/consts/MusicConst.java
+++ src/main/java/com/vae1970/tool/consts/MusicConst.java
@@ -6,12 +6,33 @@ package com.vae1970.tool.consts;
  */
 public class MusicConst {
 
+    /**
+     * 网易云域名
+     */
     private static final String DOMAIN = "http://127.0.0.1:8003";
+    /**
+     * 手机登录
+     */
     public static final String LOGIN_URL = DOMAIN + "/login/cellphone";
+    /**
+     * 刷新登录
+     */
     public static final String REFRESH_LOGIN_URL = DOMAIN + "/login/refresh";
+    /**
+     * 用户歌单列表
+     */
     public static final String PLAYLIST_URL = DOMAIN + "/user/playlist";
+    /**
+     * 歌单详情
+     */
     public static final String PLAYLIST_DETAIL_URL = DOMAIN + "/playlist/detail";
+    /**
+     * 歌单添加或删除歌曲
+     */
     public static final String PLAYLIST_TRACK_URL = DOMAIN + "/playlist/tracks";
+    /**
+     * 登录状态
+     */
     public static final String LOGIN_STATUS_URL = DOMAIN + "/login/status";
 
 }
